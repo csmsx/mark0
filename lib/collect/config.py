ecd9ba1db6b20bdde0e317e256d5b62bb58d5839
Eric Platon <zaraki@gmx.com>
2017-06-06 03:43:59 +0000

    Fix name of config variable after refactoring.

--- lib/collect/config.py
+++ lib/collect/config.py
@@ -3,4 +3,4 @@ import os
 
 LEDS_D_RUN_DIR = '/var/log/mark0/leds.d'
 mkpath(LEDS_D_RUN_DIR)
-SERVER_ADDRESS = os.path.sep.join([LEDS_D_RUN_DIR, 'leds_d_socket'])
+LEDS_D_ADDRESS = os.path.sep.join([LEDS_D_RUN_DIR, 'leds_d_socket'])
