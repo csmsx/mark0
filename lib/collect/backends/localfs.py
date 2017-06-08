d7076f042027423ad2d94551eab86d11420acb8c
Eric Platon <zaraki@gmx.com>
2017-06-08 06:37:44 +0000

    Fix file write mode.

--- lib/collect/backends/localfs.py
+++ lib/collect/backends/localfs.py
@@ -1,5 +1,5 @@
 import lib.collect.config as config
 
 def record(payload):
-    with open(config.COLLECT_API_LOG, 'w+') as f:
-        f.write(to_save)
+    with open(config.COLLECT_API_LOG, 'a') as f:
+        f.write(payload + "\n")
