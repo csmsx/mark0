f39f7d64ba8ca8051b24407811239f960cc6f561
Eric Platon <zaraki@gmx.com>
2017-06-08 06:33:17 +0000

    Fix bug in module selection.

--- lib/collect/backend.py
+++ lib/collect/backend.py
@@ -1,6 +1,9 @@
 import lib.collect.config as config
 
-if config.BACKEND == 'dynamodb':
-    import lib.collect.backends.dymamodb as api
-else:
+try:
+    if config.BACKEND == 'dynamodb':
+        import lib.collect.backends.dymamodb as api
+    else:
+        import lib.collect.backends.localfs as api
+except AttributeError:
     import lib.collect.backends.localfs as api
